6207895199adca738206a9399d6bb860105fb033
amanda-santos <amandasf0200@gmail.com>
2022-09-07 18:23:17 -0300

    feat: 💄 add colors and theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 import { ReactElement } from "react";
+import { ThemeProvider } from "styled-components";
+
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
 
 export const App = (): ReactElement => {
-  return <div>Github Blog</div>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 };
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,24 @@
+export const defaultTheme = {
+  colors: {
+    "base-primary": "#3294F8",
+    "base-title": "#E7EDF4",
+    "base-subtitle": "#C4D4E3",
+    "base-text": "#AFC2D4",
+    "base-span": "#7B96B2",
+    "base-label": "#3A536B",
+    "base-border": "#1C2F41",
+    "base-post": "#112131",
+    "base-profile": "#0B1B2B",
+    "base-background": "#071422",
+    "base-input": "#040F1A",
+  },
+  breakpoints: {
+    sm: "640px",
+    md: "768px",
+    lg: "1024px",
+    xl: "1280px",
+    "2xl": "1536px",
+    "3xl": "1920px",
+    "4xl": "2560px",
+  },
+} as const;
